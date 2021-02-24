49bb65acb8b1db8a57d99fa1bb61b934a6493fdc
MuntasirMac <Muntasir120@gmail.com>
2021-02-25 00:00:58 +0600

    Final Deploy

--- kurs/settings.py
+++ kurs/settings.py
@@ -9,11 +9,27 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
+import os
+
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,7 +95,7 @@ WSGI_APPLICATION = 'kurs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR + '/db.sqlite3',
     }
 }
 
@@ -120,4 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
--- kurstrack/admin.py
+++ kurstrack/admin.py
@@ -1,10 +1,71 @@
 from django.contrib import admin
 
 # Register your models here.
-#from kurstrack.models import *
+from kurstrack.models import *
 from django.apps import apps
+
+class AwardingBodyAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+	list_filter = ('name')
+	oredering = ('name')
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('category_id', 'category')
+	list_filter = ('category')
+	oredering = ('category')
+
+class LevelAdmin(admin.ModelAdmin):
+	list_display = ('level_id', 'level_name')
+	list_filter = ('level_name')
+	oredering = ('level_name')
+
+class InstructorinfoAdmin(admin.ModelAdmin):
+	list_display = ('instructor_id', 'first_name', 'last_name', 'profession', 'address', 'phone', 'email', 'website')
+	list_filter = ('first_name', 'last_name', 'profession', 'address', 'phone', 'email', 'website')
+	oredering = ('first_name', 'last_name', 'profession', 'address')
+
+class RegisterinfoAdmin(admin.ModelAdmin):
+	list_display = ('register_id', 'first_name', 'last_name', 'profession', 'address', 'phone', 'email', 'website')
+	list_filter = ('first_name', 'last_name', 'profession', 'address', 'phone', 'email', 'website')
+	oredering = ('first_name', 'last_name', 'profession', 'address')
+
+class CertificateTypeAdmin(admin.ModelAdmin):
+	list_display = ('type_id', 'price', 'type')
+	list_filter = ('type')
+	oredering = ('type_id')
+
+class CoursedesignAdmin(admin.ModelAdmin):
+	list_display = ('design_id', 'design_type')
+	list_filter = ('level_name')
+	oredering = ('level_name')
+
+class CartAdmin(admin.ModelAdmin):
+	list_display = ('stud_id', 'description')
+	list_filter = ('stud_id')
+	oredering = ('stud_id')
+
+class CertificationAdmin(admin.ModelAdmin):
+	list_display = ('certification_id', 'certification_name', 'type')
+	list_filter = ('certification_name', 'type')
+	oredering = ('type')
+
+class WebcourseinfoAdmin(admin.ModelAdmin):
+	list_display = ('w_course_id', 'title', 'rating', 'duration', 'original_price', 'offer_price', 'manipulated_or_not', 'career_path', 'quality', 'quality_type', 'certification')
+	list_filter = ('quality', 'quality_type', 'manipulated_or_not', 'career_path', 'category', 'rating', 'certification')
+	oredering = ('title', 'quality_type', 'offer_price', 'original_price')
+
+class CoursemoduleAdmin(admin.ModelAdmin):
+	list_display = ('module_type', 'module')
+	list_filter = ('module_type')
+	oredering = ('module')
+
+class LevelAdmin(admin.ModelAdmin):
+	list_display = ('order_no', 'w_course', 'quantity', 'total_price', 'shipping_method', 'payment_method', 'id', 'gift_or_not')
+	list_filter = ('shipping_method', 'payment_method', 'gift_or_not')
+	oredering = ('level_name')
+
 
 app = apps.get_app_config('kurstrack')
 
 for model_name, model in app.models.items():
-    admin.site.register(model)
+    admin.site.register(model,modelAdmin)
