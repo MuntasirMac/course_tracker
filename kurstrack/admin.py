e6bbe4a7fd3989d4b80e9e46fdf50b9c6a73f495
MuntasirMac <30590097+MuntasirMac@users.noreply.github.com>
2021-02-26 12:22:52 +0600

    List View Added
    List View Added

--- kurstrack/admin.py
+++ kurstrack/admin.py
@@ -1,71 +1,18 @@
 from django.contrib import admin
-
+admin.autodiscover()
 # Register your models here.
-# from kurstrack.models import *
+# from .models import *
 from django.apps import apps
-
-class AwardingBodyAdmin(admin.ModelAdmin):
-	list_display = ('id', 'name')
-	list_filter = ('name')
-	oredering = ('name')
-
-class CategoryAdmin(admin.ModelAdmin):
-	list_display = ('category_id', 'category')
-	list_filter = ('category')
-	oredering = ('category')
-
-class LevelAdmin(admin.ModelAdmin):
-	list_display = ('level_id', 'level_name')
-	list_filter = ('level_name')
-	oredering = ('level_name')
-
-class InstructorinfoAdmin(admin.ModelAdmin):
-	list_display = ('instructor_id', 'first_name', 'last_name', 'profession', 'address', 'phone', 'email', 'website')
-	list_filter = ('first_name', 'last_name', 'profession', 'address', 'phone', 'email', 'website')
-	oredering = ('first_name', 'last_name', 'profession', 'address')
-
-class RegisterinfoAdmin(admin.ModelAdmin):
-	list_display = ('register_id', 'first_name', 'last_name', 'profession', 'address', 'phone', 'email', 'website')
-	list_filter = ('first_name', 'last_name', 'profession', 'address', 'phone', 'email', 'website')
-	oredering = ('first_name', 'last_name', 'profession', 'address')
-
-class CertificateTypeAdmin(admin.ModelAdmin):
-	list_display = ('type_id', 'price', 'type')
-	list_filter = ('type')
-	oredering = ('type_id')
-
-class CoursedesignAdmin(admin.ModelAdmin):
-	list_display = ('design_id', 'design_type')
-	list_filter = ('level_name')
-	oredering = ('level_name')
-
-class CartAdmin(admin.ModelAdmin):
-	list_display = ('stud_id', 'description')
-	list_filter = ('stud_id')
-	oredering = ('stud_id')
-
-class CertificationAdmin(admin.ModelAdmin):
-	list_display = ('certification_id', 'certification_name', 'type')
-	list_filter = ('certification_name', 'type')
-	oredering = ('type')
-
-class WebcourseinfoAdmin(admin.ModelAdmin):
-	list_display = ('w_course_id', 'title', 'rating', 'duration', 'original_price', 'offer_price', 'manipulated_or_not', 'career_path', 'quality', 'quality_type', 'certification')
-	list_filter = ('quality', 'quality_type', 'manipulated_or_not', 'career_path', 'category', 'rating', 'certification')
-	oredering = ('title', 'quality_type', 'offer_price', 'original_price')
-
-class CoursemoduleAdmin(admin.ModelAdmin):
-	list_display = ('module_type', 'module')
-	list_filter = ('module_type')
-	oredering = ('module')
-
-class LevelAdmin(admin.ModelAdmin):
-	list_display = ('order_no', 'w_course', 'quantity', 'total_price', 'shipping_method', 'payment_method', 'id', 'gift_or_not')
-	list_filter = ('shipping_method', 'payment_method', 'gift_or_not')
-	oredering = ('level_name')
-
 
 app = apps.get_app_config('kurstrack')
 
 for model_name, model in app.models.items():
-    admin.site.register(model)
+    model_admin = type(model_name + "Admin", (admin.ModelAdmin,), {})
+
+    model_admin.list_display = model.admin_list_display if hasattr(model, 'admin_list_display') else tuple([field.name for field in model._meta.fields])
+    model_admin.list_filter = model.admin_list_filter if hasattr(model, 'admin_list_filter') else model_admin.list_display
+    model_admin.list_display_links = model.admin_list_display_links if hasattr(model, 'admin_list_display_links') else ()
+    model_admin.list_editable = model.admin_list_editable if hasattr(model, 'admin_list_editable') else ()
+    model_admin.search_fields = model.admin_search_fields if hasattr(model, 'admin_search_fields') else ()
+
+    admin.site.register(model, model_admin)
